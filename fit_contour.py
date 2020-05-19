a71479846fc1f5531ad7964bf0c8b802d2390043
AlejandraRamirez <ma.ramirez@uniandes.edu.co>
2020-05-18 19:38:05 -0500

    AjusteUpdate

--- fit_contour.py
+++ fit_contour.py
@@ -1,25 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+import math
+
+#math.log(number, base)
+#natural logarithm
+#np.log(number)
 
 def fun(x,y,a,b):
-    #return (a*x)/(float(b*y))
-    return a*y-b*x
+    #return (a*y**0.45) - (b*x**0.45)
+    return (a*math.log(y,2)) - (b*math.log(x,2))
 
+"""
 def fun1(data,a,b):
     return a*data[1] - b*data[0]
+"""
 
 #3x3
 #KA
-KKA = np.linspace(1,3,3)
+KKA = np.linspace(1,18,18)
 
 #KB
-KKB = np.linspace(1,3,3)
+KKB = np.linspace(1,18,18)
 
 #Generar grid - KKA es y KKB es x
 #Ahi hay confusion de ejes
 X, Y = np.meshgrid(KKA, KKB)
 
+"""
 Ans = np.zeros((3,3))
 
 take = [-5.858933808835822382e-05,-2.112965386516043112e-02,-3.903618175338666174e-02,2.083671057859595374e-02,-1.891626158611038784e-03,-2.965555453410631537e-02,3.772909134794374258e-02,2.811379580390168872e-02,-9.342721789747449523e-04]
@@ -31,6 +39,7 @@ for i in range(3):
        z+=1
 
 print Ans
+
 
 ink = np.array([[1.0,2.0,3.0,1.0,2.0,3.0,1.0,2.0,3.0],[1,1,1,2,2,2,3,3,3]])
 print ink[1]
@@ -51,26 +60,28 @@ print covAA
 avg = (mAA[0]+mAA[1])/2.0
 print "Avg"
 print avg
+"""
 
 Fi = np.ones((len(KKA),len(KKB)))
 
 for i in range(len(KKA)):
     for j in range(len(KKB)):
         #print inI
-        Fi[i,j] = fun(KKB[j], KKA[i],0.02,0.02)
+        Fi[i,j] = fun(KKB[j], KKA[i],0.017,0.017)
         #Fi[i,j] = fun(KKB[j], KKA[i],1,1)
         print Fi[i,j], KKB[j], KKA[i]
 
 
-plt.contourf(X, Y, Fi, 16, cmap="RdBu_r")
+plt.contourf(X, Y, Fi, 18, cmap="RdBu_r")
 plt.colorbar()
 plt.savefig("Ajuste.png")
-plt.show()
+#plt.show()
 plt.clf()
 
-
+"""
 plt.contourf(X, Y, Ans, 16, cmap="RdBu_r")
 plt.colorbar()
 #plt.savefig("y_x2.png")
 #plt.show()
 plt.clf()
+"""
