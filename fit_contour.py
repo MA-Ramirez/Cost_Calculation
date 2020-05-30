4bb1336dcd350ea4ed11eb03246c85529d678073
AlejandraRamirez <ma.ramirez@uniandes.edu.co>
2020-05-30 16:39:41 -0500

    Fit_Contour

--- fit_contour.py
+++ fit_contour.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-import math
 
 #math.log(number, base)
 #natural logarithm
@@ -9,79 +8,182 @@ import math
 
 def fun(x,y,a,b):
     #return (a*y**0.45) - (b*x**0.45)
-    return (a*math.log(y,2)) - (b*math.log(x,2))
+    return (a*np.log(y)) - (b*np.log(x))
 
-"""
+
 def fun1(data,a,b):
-    return a*data[1] - b*data[0]
-"""
+    return a*np.log(data[0]) - b*np.log(data[1])
 
-#3x3
-#KA
-KKA = np.linspace(1,18,18)
+#Normal
+#kh2 = np.array([2,6,10,14,18])
+#kh3 = np.array([4,10,16,22,28,34])
+#kh4 = np.array([5,13,21,29,37,45])
 
-#KB
-KKB = np.linspace(1,18,18)
+#Long
+kh2 = np.array([2,4,6,8,10,12,14,16,18])
+kh3 = np.array([4,7,10,13,16,19,22,25,28,31,34])
+kh4 = np.array([5,9,13,17,21,25,29,33,37,41,45])
 
-#Generar grid - KKA es y KKB es x
-#Ahi hay confusion de ejes
-X, Y = np.meshgrid(KKA, KKB)
-
-"""
-Ans = np.zeros((3,3))
-
-take = [-5.858933808835822382e-05,-2.112965386516043112e-02,-3.903618175338666174e-02,2.083671057859595374e-02,-1.891626158611038784e-03,-2.965555453410631537e-02,3.772909134794374258e-02,2.811379580390168872e-02,-9.342721789747449523e-04]
-
-z =0
-for i in range(3):
-    for j in range(3):
-       Ans[i,j] = take[z]
-       z+=1
-
-print Ans
+#Short
+#kh3 = np.array([1,2,3])
 
 
-ink = np.array([[1.0,2.0,3.0,1.0,2.0,3.0,1.0,2.0,3.0],[1,1,1,2,2,2,3,3,3]])
-print ink[1]
+def fitCC(h):
+    #First column of cost matrix
+    ColIni = np.genfromtxt("Results/DataCost_"+str(h)+"_W.txt", delimiter=",",usecols=0)
+
+    #The cost matrix in nxn, so #columns = #rows
+    #Lenght of first colum
+    n = np.size(ColIni)
+
+    #Define size of matrix
+    if h==2:
+        #KA
+        KKA = kh2
+        #KB
+        KKB = kh2
+        kh = kh2
+    elif h==23:
+        #KA
+        KKA = kh23
+        #KB
+        KKB = kh23
+        kh = kh23
+    elif h==27:
+        #KA
+        KKA = kh27
+        #KB
+        KKB = kh27
+        kh = kh27
+    elif h==3:
+        #KA
+        KKA = kh3
+        #KB
+        KKB = kh3
+        kh = kh3
+    elif h==4:
+        #KA
+        KKA = kh4
+        #KB
+        KKB = kh4
+        kh = kh4
 
 
-    #Fit lineal A
+    #Generate grid - KKA is Y and KKB is X
+    X, Y = np.meshgrid(KKA, KKB)
+
+    for i in range(1,n):
+        Col = np.genfromtxt("Results/DataCost_"+str(h)+"_W.txt", delimiter=",",usecols=i)
+        ColIni = np.append(ColIni, Col)
+
+    Ans = np.zeros((n,n))
+
+    z=0
+    for j in range(n):
+        for i in range(n):
+           Ans[i,j] = ColIni[z]
+           z+=1
+
+    #print Ans
+
+    fir = []
+    sec = []
+
+    for i in kh:
+        for j in kh:
+            fir.append(j)
+            sec.append(i)
+
+    ink = np.array([fir,sec])
+
+    #Fit Contour
 
     #Retorna pendiente de recta y varianza (same for A and B)
     #LA y LB tienen mismo len
-mAA, covAA = curve_fit(fun1, ink, take)
-#mA = mAA[0]
-#cA = mAA[1]
-#covA = covAA[0][0]
+    mAA, covAA = curve_fit(fun1, ink, ColIni)
+    p1 = mAA[0]
+    p2 = mAA[1]
 
-print "Param"
-print mAA
-print covAA
-avg = (mAA[0]+mAA[1])/2.0
-print "Avg"
-print avg
-"""
+    print "Param"
+    print mAA
+    print covAA
+    Cavg = (p1+p2)/2.0
+    print "CAvg"
+    print Cavg
 
-Fi = np.ones((len(KKA),len(KKB)))
+    #Print in Cost cte file
+    text_file = open("Results/Fit/CCte.csv", "a+")
+    n = text_file.write(str(h)+","+ str(Cavg)+"\n")
+    text_file.close()
 
-for i in range(len(KKA)):
-    for j in range(len(KKB)):
-        #print inI
-        Fi[i,j] = fun(KKB[j], KKA[i],0.017,0.017)
-        #Fi[i,j] = fun(KKB[j], KKA[i],1,1)
-        print Fi[i,j], KKB[j], KKA[i]
+    #Print Fit Var in file
+    text_file = open("Results/Fit/Var_Fit.csv", "a+")
+    n = text_file.write(str(h)+","+str(covAA[0][0])+","+str(covAA[0][1])+"\n")
+    text_file.close()
 
 
-plt.contourf(X, Y, Fi, 18, cmap="RdBu_r")
-plt.colorbar()
-plt.savefig("Ajuste.png")
-#plt.show()
-plt.clf()
+    Fi = np.ones((len(KKA),len(KKB)))
 
-"""
-plt.contourf(X, Y, Ans, 16, cmap="RdBu_r")
-plt.colorbar()
-#plt.savefig("y_x2.png")
-#plt.show()
-plt.clf()
-"""
+    for i in range(len(KKA)):
+        for j in range(len(KKB)):
+            Fi[i,j] = fun(KKB[j], KKA[i],p1,p2)
+
+    if h ==2:
+        #Med and long
+        L =[-0.056,-0.048,-0.04,-0.032,-0.024,-0.016,-0.008,0.0,0.008,0.016,0.024,0.032,0.04,0.048,0.056]
+    elif h ==3:
+        #Med
+        #L = 16
+        #Long
+        L = [-0.105,-0.09,-0.075,-0.06,-0.045,-0.03,-0.015,0.0,0.015,0.03,0.045,0.06,0.075,0.09,0.105]
+        #Short
+        #L = [-0.04,-0.035,-0.03,-0.025,-0.02,-0.015,-0.01,-0.005,0.0,0.005,0.01,0.015,0.02,0.025,0.03,0.035,0.04]
+    elif h ==4:
+        #Med
+        #L = [-0.175,-0.15,-0.125,-0.1,-0.075,-0.05,-0.025,0.0,0.025,0.05,0.075,0.1,0.125,0.15,0.175]
+        #Long
+        L=[-0.18,-0.16,-0.14,-0.12,-0.1,-0.08,-0.06,-0.04,-0.02,0.0,0.02,0.04,0.06,0.08,0.1,0.12,0.14,0.16,0.18]
+
+    """
+    plt.title("Fit Cost for plasmids type A ($h$ = "+str(h)+")")
+    plt.contourf(X, Y, Fi, L, cmap="RdBu_r")
+    plt.colorbar()
+    plt.xlabel("$K_{A}$")
+    plt.ylabel("$K_{B}$")
+    plt.xticks(kh)
+    plt.yticks(kh)
+    plt.savefig("Results/Fit/Fit_"+str(h)+"_W.png")
+    #plt.show()
+    plt.clf()
+
+    plt.title("Check Cost for plasmids type A ($h$ = "+str(h)+")")
+    plt.contourf(X, Y, Ans, 16, cmap="RdBu_r")
+    plt.colorbar()
+    plt.xlabel("$K_{A}$")
+    plt.ylabel("$K_{B}$")
+    plt.xticks(kh)
+    plt.yticks(kh)
+    plt.savefig("Results/Fit/Check_"+str(h)+"_W.png")
+    #plt.show()
+    plt.clf()
+    """
+
+fitCC(2)
+fitCC(3)
+fitCC(4)
+
+
+#Fit of ctes of fit function
+def Fit_Cte():
+    HS = np.genfromtxt("Results/Fit/CCte.csv", delimiter=",",usecols=0)
+    CV = np.genfromtxt("Results/Fit/CCte.csv", delimiter=",",usecols=1)
+
+    plt.title("Fit Cost Constant ($A$) vs Hill Coefficient ($h$)")
+    plt.plot(HS,CV,c="r")
+    plt.xlabel("Hill Coefficient ($h$)")
+    plt.ylabel("Fit cost constant ($A$)")
+    plt.xticks(HS)
+    plt.savefig("Results/Fit/Cte_Fit.png")
+    plt.clf()
+
+#Fit_Cte()
